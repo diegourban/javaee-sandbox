3cf6682920c1975a31e3970be61127c48743d42c
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-07-04 20:04:54 -0300

    carrinho session scoped

---
+++ src/main/java/org/javaee/sandbox/bean/CarrinhoComprasBean.java
@@ -0,0 +1,33 @@
+package org.javaee.sandbox.bean;
+
+import java.util.List;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import org.javaee.sandbox.dao.LivroDao;
+import org.javaee.sandbox.model.CarrinhoCompras;
+import org.javaee.sandbox.model.CarrinhoItem;
+import org.javaee.sandbox.model.Livro;
+
+@Model
+public class CarrinhoComprasBean {
+
+	@Inject
+	private LivroDao livroDao;
+
+	@Inject
+	private CarrinhoCompras carrinho;
+
+	public String add(Integer livroId) {
+		final Livro livro = livroDao.buscarPorId(livroId);
+		final CarrinhoItem carrinhoItem = new CarrinhoItem(livro);
+		carrinho.add(carrinhoItem);
+
+		return "/carrinho?faces-redirect=true";
+	}
+	
+	public List<CarrinhoItem> getItens() {
+		return carrinho.getItens();
+	}
+}
